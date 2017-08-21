e84ca45cd204daca28d5a3446f30ffd6d4b5aa5c
Yomguithereal <guillaumeplique@gmail.com>
2017-08-21 17:34:27 +0200

    Regularizing disjoint-set

--- disjoint-set.js
+++ disjoint-set.js
@@ -1,4 +1,3 @@
-/* eslint no-unused-vars: 0 */
 /**
  * Mnemonist Disjoint Set
  * =======================
@@ -7,29 +6,9 @@
  */
 var iterateOver = require('./utils/iterate.js');
 
-function incrementalId() {
-  var i = 0;
-
-  return function() {
-    return i++;
-  };
-}
-
-function find(parents, i) {
-
-  do {
-    i = parents[i];
-  } while (parents[i] !== i);
-
-  return i;
-}
-
-function union(parents, i, j) {
-  var ip = find(parents, i),
-      jp = find(parents, j);
-
-  parents[ip] = jp;
-}
+// TODO: do we have path compression?
+// TODO: path compression means we re-attach to the root each time this is possible
+// TODO: use the python version to polish
 
 /**
  * DisjointSet.
@@ -48,31 +27,153 @@ function DisjointSet() {
 DisjointSet.prototype.clear = function() {
 
   // Properties
-  this.id = incrementalId();
   this.size = 0;
+  this.dimension = 0;
   this.items = new Map();
+  this.indexToItems = [];
   this.parents = [];
+  this.ranks = [];
 };
 
 /**
  * Method used to add an item to the set.
  *
  * @param  {any} item - Item to add.
- * @return {undefined}
+ * @return {DisjointSet}
  */
-DisjointSet.prototype.add = function() {
+function add(set, item) {
+  var index = set.items.get(item);
 
+  if (typeof index === 'number')
+    return index;
+
+  index = set.parents.length;
+  set.indexToItems.push(item);
+  set.items.set(item, index);
+  set.parents.push(index);
+  set.ranks.push(0);
+
+  set.size++;
+  set.dimension++;
+
+  return index;
+}
+
+DisjointSet.prototype.add = function(item) {
+  add(this, item);
+  return this;
+};
+
+/**
+ * Method used to find the set's root for the given item.
+ *
+ * @param  {any}    i - Item.
+ * @return {number}
+ */
+function find(parents, i) {
+  do {
+    i = parents[i];
+  } while (parents[i] !== i);
+
+  return i;
+}
+
+DisjointSet.prototype.find = function(i) {
+
+  // TODO: should probably not add
+  return find(this.parents, add(this, i));
 };
 
 /**
  * Method used to join two items.
  *
- * @param  {any} A - First item.
- * @param  {any} B - Second item.
- * @return {undefined}
+ * @param  {any} i - First item.
+ * @param  {any} j - Second item.
+ * @return {DisjointSet}
  */
-DisjointSet.prototype.union = function() {
+DisjointSet.prototype.union = function(i, j) {
+  var iIndex = add(this, i),
+      jIndex = add(this, j);
 
+  var iParent = find(this.parents, iIndex),
+      jParent = find(this.parents, jIndex);
+
+  // Elements are already in the same set
+  if (iParent === jParent)
+    return this;
+
+  var iRank = this.ranks[i],
+      jRank = this.ranks[j];
+
+  this.dimension--;
+
+  // If i's rank is less than j's
+  if (iRank < jRank) {
+
+    // i comes under j
+    this.parents[iParent] = jParent;
+  }
+
+  // If j's rank is less than i's
+  else if (jRank < iRank) {
+
+    // j comes under i
+    this.parents[jParent] = iParent;
+  }
+
+  // If their ranks are the same
+  else {
+    this.parents[iParent] = jParent;
+    this.ranks[jParent]++;
+  }
+
+  return this;
+};
+
+/**
+ * DisjointSet Iterator class.
+ */
+function DisjointSetIterator(next) {
+  this.next = next;
+}
+
+/**
+ * Method returning an iterator over the contained sets.
+ *
+ * @return {DisjointSetIterator}
+ */
+DisjointSet.prototype.sets = function() {
+  return new DisjointSetIterator(function() {
+
+  });
+};
+
+/**
+ * Convenience known methods.
+ */
+DisjointSet.prototype.inspect = function() {
+  var setsIndex = {},
+      sets = [];
+
+  var i, l, s;
+
+  for (i = 0, l = this.parents.length; i < l; i++) {
+    s = find(this.parents, i);
+
+    setsIndex[s] = setsIndex[s] || [];
+    setsIndex[s].push(this.indexToItems[i]);
+  }
+
+  for (s in setsIndex)
+    sets.push(setsIndex[s]);
+
+  // Trick so that node displays the name of the constructor
+  Object.defineProperty(sets, 'constructor', {
+    value: DisjointSet,
+    enumerable: false
+  });
+
+  return sets;
 };
 
 /**
@@ -83,7 +184,13 @@ DisjointSet.prototype.union = function() {
  * @return {DisjointSet}
  */
 DisjointSet.from = function(iterable) {
+  var set = new DisjointSet();
 
+  iterateOver(iterable, function(value) {
+    set.add(value);
+  });
+
+  return set;
 };
 
 /**
