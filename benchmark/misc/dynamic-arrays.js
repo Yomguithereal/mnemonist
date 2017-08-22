5388f6b5b22c5250f432e6486aa1d0725b526eff
Yomguithereal <guillaumeplique@gmail.com>
2017-08-22 17:08:29 +0200

    Benchmark

---
+++ benchmark/misc/dynamic-arrays.js
@@ -0,0 +1,45 @@
+var SIZE = 100000
+
+function Wrapper() {
+  this.array = new Float64Array(SIZE);
+}
+
+Wrapper.prototype.set = function(index, value) {
+  this.array[index] = value;
+};
+
+Wrapper.prototype.get = function(index) {
+  return this.array[index];
+};
+
+suite('Dynamic Arrays', function() {
+  bench('array write', function() {
+    var array = new Float64Array(SIZE);
+
+    for (var i = 0; i < SIZE; i++)
+      array[i] = i;
+  });
+
+  bench('wrapper write', function() {
+    var wrapper = new Wrapper();
+
+    for (var i = 0; i < SIZE; i++)
+      wrapper.set(i, i);
+  });
+
+  bench('array read', function() {
+    var array = new Float64Array(SIZE),
+        j;
+
+    for (var i = 0; i < SIZE; i++)
+      j = array[i];
+  });
+
+  bench('wrapper read', function() {
+    var wrapper = new Wrapper(),
+        j;
+
+    for (var i = 0; i < SIZE; i++)
+      j = wrapper.get(i);
+  });
+});
