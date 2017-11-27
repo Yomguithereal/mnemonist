bb82041c976bca1d821f687abb98e9161add5440
Yomguithereal <guillaumeplique@gmail.com>
2017-11-27 10:22:55 +0100

    Adding function to get a number type

--- utils/typed-arrays.js
+++ utils/typed-arrays.js
@@ -33,3 +33,47 @@ exports.getPointerArray = function(size) {
 
   return Float64Array;
 };
+
+/**
+ * Function returning the minimal type able to represent the given number.
+ *
+ * @param  {number} value - Value to test.
+ * @return {TypedArrayClass}
+ */
+exports.getNumberType = function(value) {
+
+  // <= 32 bits itnteger?
+  if (value === (value | 0)) {
+
+    // Negative
+    if (Math.sign(value) === -1) {
+      if (value <= 127 && value >= -128)
+        return Int8Array;
+
+      if (value <= 32767 && value >= -32768)
+        return Int16Array;
+
+      return Int32Array;
+    }
+    else {
+
+      if (value <= 255)
+        return Uint8Array;
+
+      if (value <= 65535)
+        return Uint16Array;
+
+      return Uint32Array;
+    }
+  }
+
+  // 53 bits integer
+  if (Math.round(value) === value)
+    return Float64Array;
+
+  if (Math.fround(value) === value)
+    return Float32Array;
+
+  return Float64Array;
+
+};
