78125d9326c6fa9b8f2051dcdda9b2d59a361d1a
Yomguithereal <guillaumeplique@gmail.com>
2017-11-28 17:15:23 +0100

    Beta version

---
+++ benchmark/interval-tree/performance.js
@@ -0,0 +1,36 @@
+var StaticIntervalTree = require('../../static-interval-tree');
+var random = require('pandemonium/random');
+
+var SIZE = 5000000;
+var MAX = 5000;
+var INTERVALS = new Array(SIZE);
+
+var s, e, r, i, p;
+
+for (i = 0; i < SIZE; i++) {
+  s = random(0, MAX - 2);
+  e = random(s + 1, MAX);
+
+  INTERVALS[i] = [s, e];
+}
+
+console.time('StaticIntervalTree.from');
+var tree = StaticIntervalTree.from(INTERVALS);
+console.timeEnd('StaticIntervalTree.from');
+console.log('Tree height: ' + tree.height)
+
+p = random(0, MAX);
+
+console.time('Array point');
+r = [];
+for (i = 0; i < SIZE; i++) {
+  if (p <= INTERVALS[i][1] && p >= INTERVALS[i][0])
+    r.push(INTERVALS[i]);
+}
+console.timeEnd('Array point');
+console.log(r.length, 'intervals retrieved');
+
+console.time('StaticIntervalTree point');
+r = tree.intervalsContainingPoint(p);
+console.timeEnd('StaticIntervalTree point');
+console.log(r.length, 'intervals retrieved');
