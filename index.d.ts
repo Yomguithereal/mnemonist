e454c821a4a528e34c8fa02c7df7013383a10b02
Yomguithereal <guillaumeplique@gmail.com>
2018-03-17 16:31:45 +0100

    Starting type splitting

--- index.d.ts
+++ index.d.ts
@@ -4,34 +4,4 @@
  *
  * Gathering the library's typings.
  */
-declare module mnemonist {
-
-  /**
-   * Stack.
-   */
-  export class Stack<T> implements Iterable<T> {
-
-    // Members
-    size: number;
-
-    // Methods
-    clear(): void;
-    push(item: T): number;
-    pop(): T | undefined;
-    peek(): T | undefined;
-    forEach(callback: (item: T, index: number, stack: Stack<T>) => void, scope?: any): void;
-    toArray(): Array<T>;
-    values(): Iterator<T>;
-    entries(): Iterator<[number, T]>;
-    [Symbol.iterator](): Iterator<T>;
-    toString(): string;
-    toJSON(): Array<T>;
-    inspect(): any;
-
-    // Statics
-    static from<I>(iterable: Iterable<I>): Stack<I>;
-    static of<I>(...items: Array<I>): Stack<I>;
-  }
-}
-
-export = mnemonist;
+export {default as Stack} from './stack';
---
+++ stack.d.ts
@@ -0,0 +1,27 @@
+/**
+ * Mnemonist Stack Typings
+ * ========================
+ */
+export default class Stack<T> implements Iterable<T> {
+
+  // Members
+  size: number;
+
+  // Methods
+  clear(): void;
+  push(item: T): number;
+  pop(): T | undefined;
+  peek(): T | undefined;
+  forEach(callback: (item: T, index: number, stack: Stack<T>) => void, scope?: any): void;
+  toArray(): Array<T>;
+  values(): Iterator<T>;
+  entries(): Iterator<[number, T]>;
+  [Symbol.iterator](): Iterator<T>;
+  toString(): string;
+  toJSON(): Array<T>;
+  inspect(): any;
+
+  // Statics
+  static from<I>(iterable: Iterable<I>): Stack<I>;
+  static of<I>(...items: Array<I>): Stack<I>;
+}
