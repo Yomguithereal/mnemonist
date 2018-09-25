3a4af8e458a4c8db4e7b2a5217d2a9ee36549ded
Yomguithereal <guillaumeplique@gmail.com>
2018-09-25 11:37:32 +0200

    Using updated obliterator

--- utils/iterables.js
+++ utils/iterables.js
@@ -4,6 +4,8 @@
  *
  * Harmonized iteration helpers over mixed iterable targets.
  */
+var forEach = require('obliterator/foreach');
+
 var isTypedArray = require('./typed-arrays.js').isTypedArray;
 
 /**
@@ -15,60 +17,6 @@ var isTypedArray = require('./typed-arrays.js').isTypedArray;
  */
 function isArrayLike(target) {
   return Array.isArray(target) || isTypedArray(target);
-}
-
-/**
- * Function used to iterate in a similar way over JavaScript iterables,
- * plain objects & arrays.
- *
- * @param  {any}      target - Iteration target.
- * @param  {function} callback - Iteration callback.
- */
-function iterate(target, callback) {
-  var iterator, k, i, l, s;
-
-  // The target is an array or a string or function arguments
-  if (
-    isArrayLike(target) ||
-    typeof target === 'string' ||
-    target.toString() === '[object Arguments]'
-  ) {
-    for (i = 0, l = target.length; i < l; i++)
-      callback(target[i], i);
-    return;
-  }
-
-  // The target has a #.forEach method
-  if (typeof target.forEach === 'function') {
-    target.forEach(callback);
-    return;
-  }
-
-  // The target is iterable
-  if (typeof Symbol !== 'undefined' && Symbol.iterator in target)
-    target = target[Symbol.iterator]();
-
-  // The target is an iterator
-  if (typeof target.next === 'function') {
-    iterator = target;
-    i = 0;
-
-    while ((s = iterator.next(), !s.done)) {
-      callback(s.value, i);
-      i++;
-    }
-
-    return;
-  }
-
-  // The target is a plain object
-  for (k in target) {
-    if (target.hasOwnProperty(k)) {
-      callback(target[k], k);
-    }
-  }
-
-  return;
 }
 
 /**
@@ -101,7 +49,7 @@ function toArray(target) {
 
   var i = 0;
 
-  iterate(target, function(value) {
+  forEach(target, function(value) {
     array[i++] = value;
   });
 
@@ -112,6 +60,6 @@ function toArray(target) {
  * Exporting.
  */
 exports.isArrayLike = isArrayLike;
-exports.iterate = iterate;
+exports.iterate = forEach;
 exports.guessLength = guessLength;
 exports.toArray = toArray;
