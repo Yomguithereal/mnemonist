fed54c9f1dc99ac89823f03c548718e14259a145
Yomguithereal <guillaumeplique@gmail.com>
2019-01-28 15:02:36 +0100

    MainTitle

---
+++ docs/fosdem2019/components/MainTitle.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import components from 'mdx-deck/dist/components';
+
+export default function Title() {
+
+  return (
+    <components.h1 style={{textDecoration: 'underline'}}>
+      Developing<br />data structures<br />for JavaScript
+    </components.h1>
+  );
+};
