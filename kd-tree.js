bacf3fe64cafcf67dd0f6e6a3fb19e279c2ea7ae
Yomguithereal <guillaumeplique@gmail.com>
2020-02-27 12:58:28 +0100

    Real tree

--- kd-tree.js
+++ kd-tree.js
@@ -38,14 +38,14 @@ function squaredDistanceAxes(dimensions, axes, pivot, b) {
 
 function buildTree(dimensions, data) {
   var axes = new Array(dimensions),
-      sortedAxes = new Array(dimensions),
       values = new Array(l),
-      axis,
-      sortedAxis;
+      axis;
 
   var l = data.length;
 
   var PointerArray = typed.getPointerArray(l);
+
+  var ids = new PointerArray(l);
 
   var d, i, row;
 
@@ -53,34 +53,85 @@ function buildTree(dimensions, data) {
 
   for (d = 0; d < dimensions; d++) {
     axis = new Float64Array(l);
-    sortedAxis = new PointerArray(l);
 
     for (i = 0; i < l; i++) {
       row = data[i];
       axis[i] = row[1][d];
-      sortedAxis[i] = i;
 
-      if (f)
+      if (f) {
         values[i] = row[0];
+        ids[i] = i;
+      }
     }
 
     f = false;
     axes[d] = axis;
-    sortedAxes[d] = sortedAxis.sort(function(a, b) {
-      a = axis[a];
-      b = axis[b];
-
-      if (a < b)
-        return -1;
-
-      if (a > b)
-        return 1;
-
-      return 0;
-    });
   }
 
-  return {axes: axes, sortedAxes: sortedAxes, values: values};
+  // Building the tree
+  var pivots = new PointerArray(l),
+      lefts = new PointerArray(l),
+      rights = new PointerArray(l);
+
+  var stack = [[0, ids, -1, 0]],
+      step,
+      buffer,
+      parent,
+      direction,
+      median,
+      pivot;
+
+  i = 0;
+
+  var axisSorter = function(a, b) {
+    a = axes[d][a];
+    b = axes[d][b];
+
+    if (a < b)
+      return -1;
+
+    if (a > b)
+      return 1;
+
+    return 0;
+  };
+
+  while (stack.length !== 0) {
+    step = stack.pop();
+    d = step[0];
+    buffer = step[1];
+    parent = step[2];
+    direction = step[3];
+
+    buffer.sort(axisSorter);
+
+    median = buffer.length >>> 1;
+    pivot = buffer[median];
+    pivots[i] = pivot;
+
+    if (parent > -1) {
+      if (direction === 0)
+        lefts[parent] = i + 1;
+      else
+        rights[parent] = i + 1;
+    }
+
+    d = (d + 1) % dimensions;
+
+    // Right
+    if (median !== 0 && median !== buffer.length - 1) {
+      stack.push([d, buffer.slice(median + 1), i, 1]);
+    }
+
+    // Left
+    if (median !== 0) {
+      stack.push([d, buffer.slice(0, median), i, 0]);
+    }
+
+    i++;
+  }
+
+  return {axes: axes, values: values, pivots: pivots, lefts: lefts, rights: rights};
 }
 
 /**
@@ -229,6 +280,8 @@ KDTree.from = function(iterable) {
 module.exports = KDTree;
 
 if (require.main === module) {
+  var asciitree = require('asciitree');
+
   var D = [
     ['zero', [2, 3]],
     ['one', [5, 4]],
@@ -238,8 +291,44 @@ if (require.main === module) {
     ['five', [7, 2]]
   ];
 
-  var tree = new KDTree(2, D);
+  var tree = buildTree(2, D);
+  console.log(tree.pivots, tree.lefts, tree.rights);
+  var children = (node) => {
 
-  console.log(tree);
-  console.log(tree.nearestNeighbor([5, 4]));
+    var row = [
+      tree.lefts[node] ? (tree.lefts[node] - 1) : null,
+      tree.rights[node] ? (tree.rights[node] - 1) : null
+    ];
+
+    if (row[0] === null && row[1] === null)
+      return null;
+
+    return row;
+  };
+
+  var title = (node) => {
+    if (node === null)
+      return '';
+
+    node = tree.pivots[node];
+    return '(' + [tree.axes[0][node], tree.axes[1][node]] + ')';
+  };
+
+  console.log(asciitree(0, title, children))
+
+  // var tree = new KDTree(2, D);
+
+  // console.log(tree);
+  // console.log(tree.nearestNeighbor([5, 4]));
+
+  // var D = [];
+  // var N = 100000;
+
+  // for (var i = 0; i < N; i++) {
+  //   D.push([i, [Math.random(), Math.random()]]);
+  // }
+
+  // var tree = new KDTree(2, D);
+  // tree.nearestNeighbor([Math.random(), Math.random()]);
+  // console.log(Math.log2(N));
 }
