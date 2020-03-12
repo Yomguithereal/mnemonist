befa27d9fe9c94053fde695765ae7ffb59ce97cc
Yomguithereal <guillaumeplique@gmail.com>
2020-03-12 12:40:34 +0100

    Quicksort unit tests

--- test/sort.js
+++ test/sort.js
@@ -6,8 +6,18 @@ var assert = require('assert');
 
 var typed = require('../utils/typed-arrays.js');
 var insertion = require('../sort/insertion.js');
+var quick = require('../sort/quick.js');
 
 var DATA = [2, 7, 1, 5, 8, 9, 1, -3, 3, 18, 6];
+
+function isIncreasing(array) {
+  return array.every(function(e, i) {
+    if (!i)
+      return true;
+
+    return e > array[i - 1];
+  });
+}
 
 describe('Sort helpers', function() {
   describe('insertion', function() {
@@ -52,6 +62,39 @@ describe('Sort helpers', function() {
       indices = insertion.inplaceInsertionSortIndices(DATA.slice(), typed.indices(DATA.length), 5, 11);
 
       assert.deepEqual(Array.from(indices), [0, 1, 2, 3, 4, 7, 6, 8, 10, 5, 9]);
+    });
+  });
+
+  describe('quick', function() {
+    it('should properly sort inplace.', function() {
+
+      var data = quick.inplaceQuickSort(DATA.slice(), 0, DATA.length);
+
+      assert.deepEqual(data, [-3, 1, 1, 2, 3, 5, 6, 7, 8, 9, 18]);
+    });
+
+    it('should properly sort only a slice.', function() {
+      var data = quick.inplaceQuickSort(DATA.slice(), 0, 3);
+
+      assert.deepEqual(data, [1, 2, 7, 5, 8, 9, 1, -3, 3, 18, 6]);
+
+      data = quick.inplaceQuickSort(DATA.slice(), 3, 7);
+
+      assert.deepEqual(data, [2, 7, 1, 1, 5, 8, 9, -3, 3, 18, 6]);
+
+      data = quick.inplaceQuickSort(DATA.slice(), 5, 11);
+
+      assert.deepEqual(data, [2, 7, 1, 5, 8, -3, 1, 3, 6, 9, 18]);
+    });
+
+    it('sanity test.', function() {
+      var data = Array.from(new Array(1000), function() {
+        return Math.random();
+      });
+
+      quick.inplaceQuickSort(data, 0, data.length);
+
+      assert(isIncreasing(data), 'Array should be in increasing order');
     });
   });
 });
