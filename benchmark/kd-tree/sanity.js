da7a93850b5af24270e75dcbff52a7845e1ea292
Yomguithereal <guillaumeplique@gmail.com>
2020-03-12 14:46:44 +0100

    kd sanity test

---
+++ benchmark/kd-tree/sanity.js
@@ -0,0 +1,28 @@
+var KDTree = require('../../kd-tree.js');
+var assert = require('assert');
+
+var N = 100000;
+
+var i;
+
+var AXES = [new Float64Array(N), new Float64Array(N)];
+
+for (i = 0; i < N; i++) {
+  AXES[0][i] = Math.random();
+  AXES[1][i] = Math.random();
+}
+
+var tree = KDTree.fromAxes(AXES);
+
+for (i = 0; i < N; i++)
+  assert.strictEqual(tree.nearestNeighbor([AXES[0][i], AXES[1][i]]), i);
+
+var totalVisited = 0;
+
+for (i = 0; i < N; i++) {
+  tree.nearestNeighbor([AXES[0][i], AXES[1][i]]);
+  totalVisited += tree.visited;
+}
+
+console.log('Visited nodes avg.', totalVisited / N);
+console.log('log2', Math.log2(N));
