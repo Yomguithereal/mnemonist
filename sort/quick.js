5cca76c485d49a2862694bc3fd970e021120f733
Yomguithereal <guillaumeplique@gmail.com>
2020-03-12 12:17:45 +0100

    Drafting quicksort

---
+++ sort/quick.js
@@ -0,0 +1,62 @@
+/**
+ * Mnemonist Quick Sort
+ * =====================
+ *
+ * Quick sort related functions.
+ * Adapted from: https://alienryderflex.com/quicksort/
+ */
+var LOS = new Float64Array(64),
+    HIS = new Float64Array(64);
+
+function inplaceQuickSort(array, lo, hi) {
+  var p, i, l, r, swap;
+
+  LOS[0] = lo;
+  HIS[0] = hi;
+  i = 0;
+
+  while (i >= 0) {
+    l = LOS[i];
+    r = HIS[i] - 1;
+
+    if (l < r) {
+      p = array[l];
+
+      while (l < r) {
+        while (array[r] >= p && l < r)
+          r--;
+
+        if (l < r)
+          array[l++] = array[r];
+
+        while (array[l] <= p && l < r)
+          l++;
+
+        if (l < r)
+          array[r--] = array[l];
+      }
+
+      array[l] = p;
+      LOS[i + 1] = l + 1;
+      HIS[i + 1] = HIS[i];
+      HIS[i++] = l;
+
+      if (HIS[i] - LOS[i] > HIS[i - 1] - LOS[i - 1]) {
+        swap = LOS[i];
+        LOS[i] = LOS[i - 1];
+        LOS[i - 1] = swap;
+
+        swap = HIS[i];
+        HIS[i] = HIS[i - 1];
+        HIS[i - 1] = swap;
+      }
+    }
+    else {
+      i--;
+    }
+  }
+
+  return array;
+}
+
+exports.inplaceQuickSort = inplaceQuickSort;
